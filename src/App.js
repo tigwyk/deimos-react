2b8c5d4cd69d138499789bceeefdfdeb8f9f50a5
Lee Ingram <lee@soylentcola.com>
2017-12-31 03:18:58 +0000

    Finally got the API lookup functioning correctly using cookies

--- src/App.js
+++ src/App.js
@@ -5,25 +5,34 @@ import 'material-components-web/material-components-web.scss';
 const PATH = 'https://deimos2147.herokuapp.com/api/v1/';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			data: [],
+		};
+	}
   render() {
-	  const { msg } = this.state;
-    return (
+	  const { data } = this.state;
+      return (
       <div className="App">
-	<h1>{msg}</h1>
+	<h1>{data.name}</h1>
       </div>
     );
   }
 
   componentDidMount() {
-	  fetch(PATH, {
+	  fetch(PATH+'character', {
 	  	headers: {
 			'Content-Type': 'application/json',
 			'Accept': 'application/json'
-		}
+		},
+		credentials: "same-origin"
 	  })
 		  .then(response => response.json())
-		  .then(data => this.SetState({ msg: data.msg }));
+		  .then(data => this.setState({ data: data }));
 		}
+		
 }
 
 export default App;
