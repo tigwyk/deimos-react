9d872c543d46ab84bf1224104a93a40dc493430a
Lee Ingram <lee@soylentcola.com>
2018-01-12 22:07:26 +0000

    Reconfigured a bunch of layout stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import "material-components-web/material-components-web.scss";
-import LayoutGrid from "@react-mdc/layout-grid";
 
 const PATH = 'api/v1/';
 
@@ -17,12 +15,38 @@ class App extends Component {
 	  const { data } = this.state;
       return (
       <div className="App">
-      <LayoutGrid style={{ width: "720px" }}>
-      <LayoutGrid.Cell span={6}>{data.name}</LayoutGrid.Cell>
-    <LayoutGrid.Cell span={4}>{data.hps}/{data.max_hps}</LayoutGrid.Cell>
-<LayoutGrid.Cell span={2}>${data.credits}</LayoutGrid.Cell>
-	</LayoutGrid>
-	</div>
+      <header className="mdc-toolbar">
+      <div className="mdc-toolbar__row">
+      <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
+      <span className="mdc-toolbar__title">Deimos 2147</span>
+      </section>
+     <section className="mdc-toolbar__section mdc-toolbar__section--align-end">
+      <a href="/character" className="material-icons mdc-toolbar__icon">account_circle</a>
+      <span className="material-icons mdc-toolbar__icon">favorite</span>
+      <span className="material-icons mdc-toolbar__icon">local_atm</span>
+      </section>
+      </div>
+      </header>
+      <div className="mdc-layout-grid">
+      <div className="mdc-layout-grid__inner">
+      <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+      <h2>{data.name}</h2>
+      </div>
+      <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+      <h2>{data.hps}/{data.max_hps}</h2>
+      </div>
+      <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+      <h2>{data.location && data.location.name}</h2>
+      </div>
+      <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+      <img data-src="holder.js/100px100?theme=lava" alt=""/>
+      </div>
+      <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+      <p>{data.location && data.location.description}</p>
+      </div>
+      </div>
+      </div>
+      </div>
     );
   }
 
